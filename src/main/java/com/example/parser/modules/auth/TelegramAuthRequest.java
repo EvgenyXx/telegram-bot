23d9336ae0908b7d26224beb8bfe089b5f8e6535
evgeny <evgenypavlov666@yandex.ru>
2026-04-24 17:17:53 +0300

    пробная версия авторизация для сайта через телеграмм

---
+++ src/main/java/com/example/parser/modules/auth/TelegramAuthRequest.java
@@ -0,0 +1,9 @@
+package com.example.parser.modules.auth;
+
+public class TelegramAuthRequest {
+    public Long id;
+    public String first_name;
+    public String username;
+    public Long auth_date;
+    public String hash;
+}
